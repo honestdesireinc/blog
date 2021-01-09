70fabce0caef0bfe973262cd2665f621828acd5e
Rightscrew <09ea114n@al.rikkyo.ac.jp>
2021-01-09 20:09:09 +0900

    ALIENWARE

--- copy.bat
+++ copy.bat
@@ -1,2 +1,1 @@
-@echo off
-start /min Xcopy /e /y C:\Users\aki3t\Desktop\github_pages\blog\public\ C:\Users\aki3t\Desktop\github_pages\honestdesireinc.github.io\
+Xcopy /e /y C:\Users\aki3t\Desktop\github_pages\blog\public\ C:\Users\aki3t\Desktop\github_pages\honestdesireinc.github.io\
