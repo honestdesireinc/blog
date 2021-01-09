004d2543fa38c27c39dd5853415af5eb348568e8
Rightscrew <09ea114n@al.rikkyo.ac.jp>
2021-01-09 12:14:15 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -24,4 +24,4 @@ git commit -m "$msg"
 # Push source and build repos.
 git push origin master
 
-cp -r /c/Users/aki3t/Desktop/github_pages/blog/public/* /c/Users/aki3t/Desktop/github_pages/honestdesireinc.github.io
+cp -r /e/github_pages/blog/public/* /e/github_pages/honestdesireinc.github.io
