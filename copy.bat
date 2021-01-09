b25be06ec536f0b85c3a2f5652aa9a2810bd5593
Rightscrew <09ea114n@al.rikkyo.ac.jp>
2021-01-09 20:07:47 +0900

    Update copy.bat

--- copy.bat
+++ copy.bat
@@ -1,1 +1,2 @@
-Xcopy /e /y C:\Users\aki3t\Desktop\github_pages\blog\public\ C:\Users\aki3t\Desktop\github_pages\honestdesireinc.github.io\
+@echo off
+start /min Xcopy /e /y C:\Users\aki3t\Desktop\github_pages\blog\public\ C:\Users\aki3t\Desktop\github_pages\honestdesireinc.github.io\
