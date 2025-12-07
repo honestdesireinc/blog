4a09417c6001f76a2142a9d761fbece93da5a1dd
honestdesireinc <09ea114n@al.rikkyo.ac.jp>
2025-12-07 23:30:53 +0900

    Dell Pro 13 Premium

--- deploy.sh
+++ deploy.sh
@@ -25,4 +25,4 @@ git commit -m "$msg"
 # Push source and build repos.
 git push origin master
 
-copy C://Users/Dell/Documents/GitHub/blog/public/ C://Users/Dell/Documents/GitHub/honestdesireinc.github.io/
+copy C://Users/dell/Documents/GitHub/blog/public/ C://Users/dell/Documents/GitHub/honestdesireinc.github.io/
